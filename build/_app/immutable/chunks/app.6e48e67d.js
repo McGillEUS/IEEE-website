dce7ac9d5225fc81ce9e50dfdb48edcbcbc261ba
1vanzhang <ivan.zhang2@mail.mcgill.ca>
2025-04-11 14:22:43 -0400

    add build files

---
+++ build/_app/immutable/chunks/app.6e48e67d.js
@@ -0,0 +1,1 @@
+const o=""+new URL("../assets/small_logo.21ba59bb.png",import.meta.url).href;export{o as L};
---
+++ build/_app/immutable/nodes/4.16e70972.js
@@ -0,0 +1,44 @@
+import{s as k,f as u,a as v,g as h,G as $,c as b,h as C,d as i,j as f,i as l,A as _,l as I,m as w,J as S}from"../chunks/scheduler.318efbbf.js";import{S as x,i as T,b as M,d as G,m as B,a as z,t as W,e as L}from"../chunks/index.3ba6a661.js";import{L as R}from"../chunks/app.6e48e67d.js";import{S as A}from"../chunks/Section.a85e6943.js";function D(y){let a,g,d,n,s,E,m,r,o;return{c(){a=I(`The McGill University IEEE Student Branch was established in April 1968
+      and is one of the largest IEEE student branches in Eastern Canada. The
+      IEEE student branch at McGill is dedicated to the development of
+      professional skills of electrical & computer engineering students and the
+      larger academic community.
+
+      `),g=u("br"),d=v(),n=u("br"),s=I(`
+
+      We are mainly a group of undergraduate and graduate ECE & CS students who
+      want to have a great time, create a friendly community, and most
+      importantly learn about research and new innovations. We offer a welcoming
+      club for networking, volunteerism, teamwork, soft skills, leadership, and
+      recognition. We are under the tutelage of the larger IEEE organization and
+      organize events and activities such as talks, workshops, competitions,
+      company tours etc. for the benefit of all ECE/CS students.
+      `),E=u("br"),m=v(),r=u("br"),o=I(`
+      The IEEE organization (Institute of Electrical and Electronics Engineers) and
+      its members inspire a global community to innovate for a better tomorrow through
+      its more than 400,000 members in more than 160 countries, and its highly cited
+      publications, conferences, technology standards, and professional and educational
+      activities. IEEE is considered the trusted “voice” for engineering, computing,
+      and technology information around the globe.`)},l(e){a=w(e,`The McGill University IEEE Student Branch was established in April 1968
+      and is one of the largest IEEE student branches in Eastern Canada. The
+      IEEE student branch at McGill is dedicated to the development of
+      professional skills of electrical & computer engineering students and the
+      larger academic community.
+
+      `),g=h(e,"BR",{}),d=b(e),n=h(e,"BR",{}),s=w(e,`
+
+      We are mainly a group of undergraduate and graduate ECE & CS students who
+      want to have a great time, create a friendly community, and most
+      importantly learn about research and new innovations. We offer a welcoming
+      club for networking, volunteerism, teamwork, soft skills, leadership, and
+      recognition. We are under the tutelage of the larger IEEE organization and
+      organize events and activities such as talks, workshops, competitions,
+      company tours etc. for the benefit of all ECE/CS students.
+      `),E=h(e,"BR",{}),m=b(e),r=h(e,"BR",{}),o=w(e,`
+      The IEEE organization (Institute of Electrical and Electronics Engineers) and
+      its members inspire a global community to innovate for a better tomorrow through
+      its more than 400,000 members in more than 160 countries, and its highly cited
+      publications, conferences, technology standards, and professional and educational
+      activities. IEEE is considered the trusted “voice” for engineering, computing,
+      and technology information around the globe.`)},m(e,t){l(e,a,t),l(e,g,t),l(e,d,t),l(e,n,t),l(e,s,t),l(e,E,t),l(e,m,t),l(e,r,t),l(e,o,t)},p:S,d(e){e&&(i(a),i(g),i(d),i(n),i(s),i(E),i(m),i(r),i(o))}}}function V(y){let a,g="IEEE McGill",d,n,s,E=`<h1 class="svelte-1rgg89a"><img src="${R}" id="logo" alt="IEEE McGill Emblem" class="svelte-1rgg89a"/>
+      IEEE McGill Student Branch</h1>`,m,r,o,e;return o=new A({props:{$$slots:{default:[D]},$$scope:{ctx:y}}}),{c(){a=u("title"),a.textContent=g,d=v(),n=u("div"),s=u("div"),s.innerHTML=E,m=v(),r=u("div"),M(o.$$.fragment),this.h()},l(t){a=h(t,"TITLE",{"data-svelte-h":!0}),$(a)!=="svelte-1kiuyy4"&&(a.textContent=g),d=b(t),n=h(t,"DIV",{id:!0,class:!0});var c=C(n);s=h(c,"DIV",{id:!0,class:!0,"data-svelte-h":!0}),$(s)!=="svelte-sm1x7h"&&(s.innerHTML=E),m=b(c),r=h(c,"DIV",{id:!0,class:!0});var p=C(r);G(o.$$.fragment,p),p.forEach(i),c.forEach(i),this.h()},h(){f(s,"id","introduction"),f(s,"class","svelte-1rgg89a"),f(r,"id","paragraph"),f(r,"class","svelte-1rgg89a"),f(n,"id","content"),f(n,"class","svelte-1rgg89a")},m(t,c){l(t,a,c),l(t,d,c),l(t,n,c),_(n,s),_(n,m),_(n,r),B(o,r,null),e=!0},p(t,[c]){const p={};c&1&&(p.$$scope={dirty:c,ctx:t}),o.$set(p)},i(t){e||(z(o.$$.fragment,t),e=!0)},o(t){W(o.$$.fragment,t),e=!1},d(t){t&&(i(a),i(d),i(n)),L(o)}}}class J extends x{constructor(a){super(),T(this,a,null,V,k,{})}}export{J as component};
